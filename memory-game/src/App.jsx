dfb90a73f660ffa450602fcf595ab50e987bdfd6
Naheel <kknaheel16@gmail.com>
2025-07-13 12:10:46 +0530

    Boilerplate

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Form from './components/Form'
+import MemoryCard from './components/MemoryCard'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const [isGameOn, setIsGameOn] = useState(false)
+    
+    function startGame(e) {
+        e.preventDefault()
+        setIsGameOn(true)
+    }
+    
+    function turnCard() {
+        console.log("Memory card clicked")
+    }
+    
+    return (
+        <main>
+            <h1>Memory</h1>
+            {!isGameOn && <Form handleSubmit={startGame} />}
+            {isGameOn && <MemoryCard handleClick={turnCard} />}
+        </main>
+    )
 }
-
-export default App
