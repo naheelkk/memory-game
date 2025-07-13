e5389b5a64aea94e1a2ab218318b913f748989f1
Naheel <kknaheel16@gmail.com>
2025-07-13 14:06:24 +0530

    fetch API

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,24 +1,40 @@
-import { useState } from 'react'
-import Form from './components/Form'
-import MemoryCard from './components/MemoryCard'
+import { useState } from "react";
+import Form from "./components/Form";
+import MemoryCard from "./components/MemoryCard";
 
 export default function App() {
-    const [isGameOn, setIsGameOn] = useState(false)
-    
-    function startGame(e) {
-        e.preventDefault()
-        setIsGameOn(true)
+  const [isGameOn, setIsGameOn] = useState(false);
+
+  async function startGame(e) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(
+        "https://emojihub.yurace.pro/api/all/category/animals-and-nature"
+      );
+      if (!res.ok) {
+        console.log("Not a Success");
+        throw new Error("Couldn't fetch 'em")
+      } else {
+        console.log("Success");
+        const data = await res.json();
+        console.log(data);
+        setIsGameOn(true);
+      }
+    } catch (e) {
+      console.log(e);
     }
-    
-    function turnCard() {
-        console.log("Memory card clicked")
-    }
-    
-    return (
-        <main>
-            <h1>Memory</h1>
-            {!isGameOn && <Form handleSubmit={startGame} />}
-            {isGameOn && <MemoryCard handleClick={turnCard} />}
-        </main>
-    )
+  }
+
+  function turnCard() {
+    console.log("Memory card clicked");
+  }
+
+  return (
+    <main>
+      <h1>Memory</h1>
+      {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} />}
+    </main>
+  );
 }
